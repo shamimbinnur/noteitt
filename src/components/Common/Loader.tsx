23aca18b973f0a484aeae19f4a529f2bafc33a1b
shamimbinnnur <sbnsbn00@gmail.com>
2022-12-19 21:02:01 +0600

    Tweaked loader

--- src/components/Common/Loader.tsx
+++ src/components/Common/Loader.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Loader = () => {
   return (
     <div className="bg-gradient-to-br from-PURPLE100 via-PURPLE50 to-CYAN100 h-screen">
-      <div className="bg-gray-800 h-full bg-opacity-80">
+      <div className="bg-gray-900 h-full bg-opacity-50">
         <div className="flex h-full justify-center items-center">
-          <div className="animate-spin-fast w-20 h-20 rounded-xl bg-gradient-to-br from-PURPLE100 via-gray-100 to-CYAN100">
+          <div className="animate-spin-fast w-24 h-24 rounded-xl bg-gradient-to-br from-PURPLE100 via-gray-100 to-CYAN100">
           </div>
         </div>
       </div>
