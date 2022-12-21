58e9593ac03a1394f7ef7dba3a4676d02e2b7cd2
shamimbinnnur <sbnsbn00@gmail.com>
2022-12-21 20:53:01 +0600

    feat: new logo

--- src/components/labels/NewNote.tsx
+++ src/components/labels/NewNote.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { BiPen } from 'react-icons/bi';
+import { FaLeaf } from 'react-icons/fa';
 
 const NewNote = () => {
   return (
-    <div className="min-w-26 rounded-2xl py-1 px-2 gap-x-1 bg-CYAN100 flex items-center ">
-      <BiPen className="w-[20px] h-[20px] text-white"/>
+    <div className="min-w-26 rounded-2xl py-1 px-2 gap-x-[6px] bg-CYAN100 flex items-center ">
+      <FaLeaf className="w-[20px] h-[20px] text-white"/>
       <div className=" flex flex-col gap-y-[1px]">
-        <p className="text-[0.60rem] font-semibold text-white leading-none">New note</p>
+        <p className="text-[0.70rem] font-semibold text-white leading-none">New note</p>
       </div>
     </div>
   )
