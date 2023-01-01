b9a3a274e350bc3bb5f7b67ff980b25dfa56dbb4
shamimbinnnur <sbnsbn00@gmail.com>
2023-01-01 18:57:35 +0600

    white bg

--- src/components/Common/Loader.tsx
+++ src/components/Common/Loader.tsx
@@ -4,49 +4,49 @@ import { motion } from 'framer-motion'
 const Loader = () => {
   return (
     <div className="bg-gradient-to-br from-PURPLE100 via-PURPLE50 to-CYAN100 h-screen">
-      <div className="bg-gray-900 h-full bg-opacity-50">
+      <div className="bg-white h-full bg-opacity-90">
           <motion.div
           initial={{opacity: 0, scale: 0}}
           animate={{opacity:1, scale: 1}}
           className="flex pt-56 justify-center items-center gap-x-2"
         >
           <motion.div
-            animate={{x: 8, scale: 0.5}}
+            animate={{scale: 1, y: 10}}
             transition={{
               repeat: Infinity,
               repeatType: "reverse",
               duration: .5
             }}
-            className="w-10 h-10 rounded-full bg-BLUE50"
+            className="w-10 h-10 rounded-full border-2 bg-BLUE50"
           >
           </motion.div>
 
           <motion.div
-            animate={{x: 8, scale: 0.5}}
+            animate={{scale: 0.9, y: 10}}
             transition={{
               repeat: Infinity,
               repeatType: "reverse",
-              duration: .6
+              duration: .5,
             }}
             className="w-10 h-10 rounded-full bg-CYAN100"
           >
           </motion.div>
 
-          <motion.div animate={{x: -8, scale: 0.5}}
+          <motion.div animate={{scale: 1.1, y: 10}}
             transition={{
               repeat: Infinity,
               repeatType: "reverse",
-              duration: .6
+              duration: .5,
             }}
             className="w-10 h-10 rounded-full bg-RED50"
           >
           </motion.div>
 
-          <motion.div animate={{x: -8, scale: 0.5}}
+          <motion.div animate={{scale: 0.9, y: 10}}
             transition={{
               repeat: Infinity,
               repeatType: "reverse",
-              duration: .5
+              duration: .5,
             }}
             className="w-10 h-10 rounded-full bg-PURPLE100"
           >
